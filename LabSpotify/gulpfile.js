4c917b8cd7c179f2d2d4bca8582e8049190bafc3
Anton Samal <anton.samal@mail.ru>
2016-10-30 15:31:27 +0300

    add comment 4 glup

--- LabSpotify/gulpfile.js
+++ LabSpotify/gulpfile.js
@@ -10,18 +10,20 @@ const concat = require('gulp-concat');
 
 gulp.task('build',function(){
 
-    gulp.src('*.js')
+    gulp.src('src/**/*.js') // берет за исходные все .js в папке src и всех ее подкаталогов
         .pipe(sourcemaps.init())
         .pipe(babel({
+
             presets: ['latest']
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('public'))
+        .pipe(gulp.dest('public')) // тут меняется выходной каталог
 
 });
+
 gulp.task('watch',function(){
 
-    gulp.watch('*.js',['build']);
+    gulp.watch('src/**/*.js',['build']); // следит за всеми .js в папке src и всех ее подкаталогов
 
 });
 gulp.task('default',['build','watch']);
