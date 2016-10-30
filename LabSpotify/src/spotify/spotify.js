b5b9deb1b279e79695481cb9f729f670d10e7362
IgorLit <igorek96by@gmail.com>
2016-10-30 18:26:41 +0300

    async/await for  single search
    It was removed caching.
    if you had the same error message: 'ReferenceError: regeneratorRuntime
    is not defined'
    
    npm install babel-polyfill --save
    
    Then insert the following require statement towards the top of the
    affected module to obtain required (generator) behaviour:
    require("babel-polyfill");

--- LabSpotify/src/spotify/spotify.js
+++ LabSpotify/src/spotify/spotify.js
@@ -4,18 +4,8 @@ var needle = Promise.promisifyAll(require("needle"));
 var config = require("nconf");
 var cache = require("./cache");
 
-function search(searchObj) {
-    return new Promise(function (resolve,reject) {
-        let arrive;
-        rememberArrival().then(time=>arrive=time)
-            .then(check=> cache.checkCache([searchObj.type],searchObj.searchString,Date.now()))
-            .then(result=>{
-                if(result) {
-                    resolve([result, arrive]);
-                }
-                else requestToSpotify(resolve,reject,searchObj,arrive);
-        });
-    })
+async function search(searchObj) {
+return await requestToSpotify(searchObj)
 }
 
 function multipleSearch(firstSearchObj,secondSearchObj,mode) {
@@ -29,14 +19,16 @@ function multipleSearch(firstSearchObj,secondSearchObj,mode) {
         return Promise.any(requests);
 
 }
-function requestToSpotify(resolve,reject,searchObj,startTime) {
-    needle.get(getUrl(searchObj.searchString,searchObj.type,searchObj.limit), function(error, response) {
-        if (error)
-            reject(error);
-        if(response) {
-            cache.addCache(searchObj.searchString,searchObj.type,response.body,new Date().setMinutes(new Date().getMinutes() + 5));
-            resolve([response.body,startTime]);
-        }
+function requestToSpotify(searchObj,startTime) {
+    return new Promise((resolve,reject)=>{
+        needle.get(getUrl(searchObj.searchString,searchObj.type,searchObj.limit), function(error, response) {
+            if (error)
+                reject(error);
+            if(response) {
+               // cache.addCache(searchObj.searchString,searchObj.type,response.body,new Date().setMinutes(new Date().getMinutes() + 5));
+                resolve([response.body,startTime]);
+            }
+        });
     });
 }
 function rememberArrival() {
