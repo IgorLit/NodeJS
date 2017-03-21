364f0d8964a96bad975d25a17dc4c9d2ad5bb4d0
igorLit <Igorek96by@gmail.com>
2017-03-21 11:49:39 +0300

    npm-package

---
+++ npm-package/index.js
@@ -0,0 +1,64 @@
+const needle = require("needle");
+const Promise = require("bluebird");
+Promise.promisifyAll(needle);
+
+function getTopRepoByUser(user) {
+    return new Promise((resolve, reject) => {
+        needle.getAsync('https://api.github.com/users/' + user + '/repos')
+            .then((response) => {
+                let repoArray = [];
+                response.body.every((item, index) => {
+                    let repo = {
+                        id: item.id,
+                        name: item.name,
+                        url: item.html_url
+                    };
+                    repoArray.push(repo);
+                    return index != 9;
+                });
+                resolve(repoArray);
+            })
+    })
+}
+function findRepoByName(name) {
+    return new Promise((resolve, reject) => {
+        needle.getAsync('https://api.github.com/search/repositories?q=' + name)
+            .then((response) => {
+                let repoArray = [];
+                response.body.items.every((item, index) => {
+                    let repo = {
+                        id: item.id,
+                        name: item.name,
+                        owner: item.owner.login,
+                        url: item.html_url
+                    };
+                    repoArray.push(repo);
+                    return index != 9;
+                });
+                resolve(repoArray);
+            })
+    })
+}
+function getLastCommits(user, repository) {
+    return new Promise((resolve, reject) => {
+        needle.getAsync('https://api.github.com/repos/' + user + '/' + repository + '/commits')
+            .then((response) => {
+                let repoArray = [];
+                response.body.every((item, index) => {
+                    let repo = {
+                        sha: item.sha,
+                        author: item.author.login,
+                        message: item.commit.message,
+                        date: item.commit.committer.date,
+                    };
+                    repoArray.push(repo);
+                    return index != 9;
+                });
+                resolve(repoArray);
+            })
+    })
+}
+
+exports.getTopRepoByUser = getTopRepoByUser;
+exports.findRepoByName = findRepoByName;
+exports.getLastCommits = getLastCommits;
